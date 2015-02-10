ff4dc9cf07e4a5ac43e320c2edf5bfec917acdd1
David King <dking@ketralnis.com>
2015-02-10 14:18:50 -0800

    Pipelines are a nice abstraction, but processes are faster for this case

--- imagecluster.py
+++ imagecluster.py
@@ -6,10 +6,10 @@ import sys
 from functools import partial
 import colorsys
 import logging
+from multiprocessing import Pool, cpu_count
 
 from PIL import Image
 import numpy as np
-import pipeline
 
 import kmeans
 from progress import progress
@@ -33,6 +33,7 @@ for x in values:
             colors_p.append((x,y,z))
 colors_p.sort()
 
+
 def distance(v1, v2):
     diffs = [(p2-p1)**2 for (p1, p2) in zip(v1, v2)]
     return math.sqrt(sum(diffs))
@@ -44,37 +45,39 @@ def closest(pix, coords):
     return which
 
 
-def getimage(fname):
+def makehist(fname):
     maxsize = (128, 128)
 
     im = Image.open(fname, 'r')
+
+    # the resizing operation in C is faster than the counting operation in Python,
+    # so this makes us much faster while losing very little data
     im.thumbnail(maxsize) # modifies in place
+
+    # get it into RGB first if it's not (e.g. pallet-based images)
     im = im.convert('RGB')
-    return im
 
-
-def makehist(im):
     hist = np.array([0] * len(colors_p))
 
     for pix in im.getdata():
+        # map every pixel to its nearest quantised colour
         try:
             pix = colorsys.rgb_to_hsv(*pix)
         except Exception:
             logging.exception("wtf? %r", pix)
         idx = closest(pix, colors_p)
+        # and build the histogram from the result
         hist[idx] += 1
 
     npixels = im.size[0] * im.size[1]
 
+    # divide by the size
     hist = np.array([float(x)/npixels for x in hist])
 
-    # divide by the size
-    #hist /= float(npixels)
-
-    return hist
+    return fname, hist
 
 
-def main(filesdir, outdir, nclusters=5):
+def main(filesdir, outdir, nclusters=10):
     hists = []
 
     fnames = os.listdir(filesdir)
@@ -83,18 +86,13 @@ def main(filesdir, outdir, nclusters=5):
               if not fn.startswith('.')
               and any(fn.lower().endswith(x) for x in exts)]
     random.shuffle(fnames)
-    #fnames = random.sample(fnames, 10)
-
-    pl = pipeline.Pipeline([
-        pipeline.Generator(fnames),
-        pipeline.MultiMapper(lambda fname: (fname, getimage(fname)), 2),
-        pipeline.Mapper(lambda tup: (tup[0], makehist(tup[1]))),
-    ], bufsize=5)
 
     maps = {}
 
-    for fname, hist in progress(pl, verbosity=1, estimate=len(fnames),
-                                key=lambda x: x[0]):
+    pool = Pool(processes=cpu_count())
+
+    for fname, hist in progress(pool.imap_unordered(makehist, fnames, chunksize=1),
+                                verbosity=1, estimate=len(fnames), key=lambda x: x[0]):
         maps[id(hist)] = fname
         hists.append(hist)
 
