103fddf3ab6c1aad1dd91dc55559ab19513a4b22
David King <dking@ketralnis.com>
2015-02-12 17:49:52 -0800

    histcache can make this faster to test

--- imagecluster.py
+++ imagecluster.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+import sqlite3
+import argparse
 import os
 import os.path
 import math
@@ -7,6 +11,7 @@ from functools import partial
 import colorsys
 import logging
 from multiprocessing import Pool, cpu_count
+import pickle
 
 from PIL import Image
 import numpy as np
@@ -15,32 +20,32 @@ import kmeans
 from progress import progress
 
 exts = ('jpg', 'png', 'pef')
-#colors = {
-#    'black': (0, 0, 0),
-#    'red': (255, 0, 0),
-#    'redgreen': (255, 255, 0),
-#    'green': (0, 255, 0),
-#    'greenblue': (0, 255, 255),
-#    'blue': (0, 0, 255),
-#    'redblue': (255, 0, 255),
-#    'white': (255, 255, 255),
-#}
-values = [0, 255/2, 255]
-colors_p = []
-for x in values:
-    for y in values:
-        for z in values:
-            colors_p.append((x,y,z))
-colors_p.sort()
+
+def make_colors():
+    values = [0, 0.5, 1.0]
+    colors = []
+    for x in values:
+        for y in values:
+            for z in values:
+                colors.append(colorsys.rgb_to_hsv(x,y,z))
+    return colors
+
+colors_p = make_colors()
 
 
 def distance(v1, v2):
+    # differences = (v2-v1)**2
+    # summ = differences.sum()
+    # dist = math.sqrt(summ)
+    # return dist
     diffs = [(p2-p1)**2 for (p1, p2) in zip(v1, v2)]
     return math.sqrt(sum(diffs))
 
 
 def closest(pix, coords):
-    dists = [(distance(pix, coord), i) for i, coord in enumerate(coords)]
+    dists = [(distance(pix, coord), i)
+             for i, coord
+             in enumerate(coords)]
     dist, which = min(dists)
     return which
 
@@ -57,7 +62,7 @@ def makehist(fname):
     # get it into RGB first if it's not (e.g. pallet-based images)
     im = im.convert('RGB')
 
-    hist = np.array([0] * len(colors_p))
+    hist = np.array([0.0] * len(colors_p))
 
     for pix in im.getdata():
         # map every pixel to its nearest quantised colour
@@ -67,48 +72,108 @@ def makehist(fname):
             logging.exception("wtf? %r", pix)
         idx = closest(pix, colors_p)
         # and build the histogram from the result
-        hist[idx] += 1
+        hist[idx] += 1.0
 
-    npixels = im.size[0] * im.size[1]
+    npixels = float(im.size[0] * im.size[1])
 
     # divide by the size
-    hist = np.array([float(x)/npixels for x in hist])
+    hist /= npixels
 
     return fname, hist
 
 
-def main(filesdir, outdir, nclusters=10):
-    hists = []
+def main(argv):
+    parser = argparse.ArgumentParser()
 
-    fnames = os.listdir(filesdir)
-    fnames = [os.path.join(filesdir, fn)
-              for fn in fnames
-              if not fn.startswith('.')
-              and any(fn.lower().endswith(x) for x in exts)]
-    random.shuffle(fnames)
+    parser.add_argument('--cache', dest='cache', default=None)
+    parser.add_argument('-n', dest='num_clusters', type=int, default=10)
 
+    parser.add_argument('indir')
+    parser.add_argument('outdir')
+
+    args = parser.parse_args()
+
+    if args.indir == '-':
+        fnames = map(lambda s: s.rstrip('\n'), sys.stdin)
+    else:
+        fnames = os.listdir(args.indir)
+        fnames = [os.path.join(args.indir, fn)
+                  for fn in fnames
+                  if not fn.startswith('.')
+                  and any(fn.lower().endswith(x) for x in exts)]
+
+    hists = {}
+
+    # after clustering, we just get the clustered coordinates back. this lets
+    # us map them back to the source fnames instead of coordinates
     maps = {}
 
-    pool = Pool(processes=cpu_count())
+    # the cache connection if we have one
+    conn = None
 
-    for fname, hist in progress(pool.imap_unordered(makehist, fnames, chunksize=1),
-                                verbosity=1, estimate=len(fnames), key=lambda x: x[0]):
-        maps[id(hist)] = fname
-        hists.append(hist)
+    if args.cache:
+        conn = sqlite3.connect(args.cache)
+        conn.text_factory = str
 
-    mu, clusters = kmeans.find_centers(hists, nclusters)
+        conn.execute("""
+                     CREATE TABLE IF NOT EXISTS histcache(fname PRIMARY KEY, hist)
+                     """)
+
+        # clean up the cache by deleting any entries that aren't present in the
+        # dataset
+        conn.execute('CREATE TEMPORARY TABLE wantfiles(fname)')
+        for fname in fnames:
+            conn.execute("INSERT INTO wantfiles(fname) VALUES(?)",
+                         (fname,))
+
+        curs = conn.cursor()
+
+        curs.execute("""
+                     DELETE FROM histcache
+                     WHERE fname NOT IN (SELECT fname FROM wantfiles)
+                     """)
+        if curs.rowcount:
+            logging.debug("Deleted %d stale entries from the histcache",
+                          curs.rowcount)
+
+        cachedhists = list(conn.execute('SELECT fname, hist FROM histcache'))
+
+        for fname, hist in cachedhists:
+            hist = pickle.loads(hist)
+            hists[fname] = hist
+            maps[id(hist)] = fname
+
+    searchfnames = [ fname for fname in fnames if fname not in hists ]
+
+    if searchfnames:
+        # this just makes the progress bar more accurate
+        random.shuffle(searchfnames)
+
+        pool = Pool(processes=cpu_count())
+
+        for fname, hist in progress(pool.imap_unordered(makehist, searchfnames, chunksize=1),
+                                    verbosity=1, estimate=len(searchfnames), key=lambda x: x[0]):
+            maps[id(hist)] = fname
+            hists[fname] = hist
+
+            if conn:
+                aspickle = pickle.dumps(hist)
+                conn.execute("INSERT INTO histcache(fname, hist) VALUES(?, ?)",
+                             (fname, aspickle))
+                conn.commit()
+
+
+    mu, clusters = kmeans.find_centers(hists.values(), args.num_clusters)
 
     for num, cluster in clusters.iteritems():
         mymu = mu[num]
-        bdir = os.path.join(outdir, '%02d' % (num,))
+        bdir = os.path.join(args.outdir, '%02d' % (num,))
         os.makedirs(bdir)
         cluster.sort(key=partial(distance, mymu))
         for i, item in enumerate(cluster):
             fname = maps[id(item)]
             toname = os.path.join(bdir, '%03d.jpg' % (i,))
-            print 'link', fname, toname
             os.link(fname, toname)
 
-indir, outdir = sys.argv[1:]
-main(indir, outdir)
+main(sys.argv)
 
